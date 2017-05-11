643721b57a25aa2b6689ccb857c0f1b45dc1e068
Naudiyal <anaudiya@9801a7ab9fad.ant.amazon.com>
2017-05-11 16:21:28 +1000

    AddedMore

---
+++ opsworks_nodejs/attributes/opsworks_nodejs.rb
@@ -0,0 +1,37 @@
+###
+# Do not use this file to override the opsworks_nodejs cookbook's default
+# attributes.  Instead, please use the customize.rb attributes file,
+# which will keep your adjustments separate from the AWS OpsWorks
+# codebase and make it easier to upgrade.
+#
+# However, you should not edit customize.rb directly. Instead, create
+# "opsworks_nodejs/attributes/customize.rb" in your cookbook repository and
+# put the overrides in YOUR customize.rb file.
+#
+# Do NOT create an 'opsworks_nodejs/attributes/opsworks_nodejs.rb' in your cookbooks. Doing so
+# would completely override this file and might cause upgrade issues.
+#
+# See also: http://docs.aws.amazon.com/opsworks/latest/userguide/customizing.html
+###
+
+include_attribute 'deploy'
+include_attribute 'opsworks_commons::default'
+
+# This value is only to give some sane initialization. This value:
+#  - May not point to the latest available version
+#  - May not be valid for the used OS
+#  - Will be overwritten by the value set on the OpsWorks NodeJS layer of your Stack
+#  - May be overwritten by a value set in your Stack's custom JSON
+default[:opsworks_nodejs][:version] = '0.12.10'
+
+default[:opsworks_nodejs][:pkgrelease] = '1'
+
+arch = RUBY_PLATFORM.match(/64/) ? 'amd64' : 'i386'
+default[:opsworks_nodejs][:deb] = "opsworks-nodejs-#{node[:opsworks_nodejs][:version]}-#{node[:opsworks_nodejs][:pkgrelease]}_#{arch}.deb"
+default[:opsworks_nodejs][:deb_url] = "#{node[:opsworks_commons][:assets_url]}/packages/#{node[:platform]}/#{node[:platform_version]}/#{node[:opsworks_nodejs][:deb]}"
+
+rhel_arch = RUBY_PLATFORM.match(/64/) ? 'x86_64' : 'i686'
+default[:opsworks_nodejs][:rpm] = "opsworks-nodejs-#{node[:opsworks_nodejs][:version]}-#{node[:opsworks_nodejs][:pkgrelease]}.#{rhel_arch}.rpm"
+default[:opsworks_nodejs][:rpm_url] = "#{node[:opsworks_commons][:assets_url]}/packages/#{node[:platform]}/#{node[:platform_version]}/#{node[:opsworks_nodejs][:rpm]}"
+
+include_attribute "opsworks_nodejs::customize"
